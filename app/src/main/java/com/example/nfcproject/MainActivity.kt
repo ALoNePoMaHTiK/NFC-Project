d4333b78f0b32f133979345b7c38b3166a1387e6
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-02-26 14:39:18 +0300

    Remove functions from activity to fragments

--- app/src/main/java/com/example/nfcproject/MainActivity.kt
+++ app/src/main/java/com/example/nfcproject/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.nfcproject
 import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NfcAdapter
 import android.os.Bundle
-import android.os.Parcelable
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nfcproject.databinding.ActivityMainBinding
@@ -16,61 +10,27 @@ var studNumber:String = ""
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainbtn:Button
-    lateinit var studNumberEditText:EditText
-
-    private var nfcAdapter: NfcAdapter? = null
-
     private var nfcTag:String = ""
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
     override fun onNewIntent(intent: Intent?){
         super.onNewIntent(intent)
         Log.d("NFCProjectTestDebug","New Intent")
-        if (intent != null) {
-            NFCHandler().processIntent(intent)
-        }
+        if (intent != null) NFCHandler().processIntent(intent)
     }
-
-    private fun readNFC(){
+     fun readNFC(){
         if (intent != null) {
             nfcTag = NFCHandler().processIntent(intent)
             Log.d("NFCProjectTestDebug",nfcTag+studNumber)
-            if (nfcTag != "" && studNumber != "") {
-                sendUserInputData()
-            }
+            if (nfcTag != "" && studNumber != "") sendUserInputData()
         }
     }
-
     private fun sendUserInputData(){
-
         Toast.makeText(applicationContext,"Данные отправлены",Toast.LENGTH_LONG).show()
-    }
-
-    private fun saveUserInputData(inputData:String){
-        if(inputValidation(inputData)){
-            studNumber = inputData.uppercase()
-            Log.d("NFCProjectTestDebug","Номер студенческого: "+studNumber)
-            /// запись в файл!!!!
-            setContentView(R.layout.activity_main)
-            Toast.makeText(applicationContext,"Данные сохранены",Toast.LENGTH_LONG).show()
-        }
-    }
-    private fun inputValidation(inputData:String): Boolean{
-        val countСharacter = 7
-        return if(countСharacter == inputData.length) {
-            true
-        }
-        else {
-            Toast.makeText(applicationContext,"Номер студенческого билета введен некоректно",Toast.LENGTH_LONG).show()
-            false
-        }
     }
 }
--- app/src/main/java/com/example/nfcproject/MainFragment.kt
+++ app/src/main/java/com/example/nfcproject/MainFragment.kt
@@ -1,59 +1,54 @@
 package com.example.nfcproject
 
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.nfcproject.databinding.FragmentMainBinding
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.SaveButton.setOnClickListener {saveButtonHendler()}
     }
+    private fun saveButtonHendler(){
+        if(inputValidation(binding.editTextStudentNumber.text.toString()))findNavController()
+            .navigate(R.id.action_mainFragment2_to_dataSendFragment)
+    }
+    private fun saveUserInputData(inputData:String){
+        if(inputValidation(inputData)){
+            studNumber = inputData.uppercase()
+            Log.d("NFCProjectTestDebug","Номер студенческого: "+studNumber)
+            /// запись в файл!!!!
+            showMessage("Данные сохранены")
+        }
+    }
+    private fun inputValidation(inputData:String): Boolean{
+        val countСharacter = 7
+        return if(countСharacter == inputData.length) {
+            true
+        }
+        else {
+            showMessage("Номер студенческого билета введен некоректно")
+            false
+        }
+    }
+    private fun showMessage(message: String) = Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+
 }
