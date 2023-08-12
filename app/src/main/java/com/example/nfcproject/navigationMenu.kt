745df62dccf140fce446cdf2f6424fc863e8ab70
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-08-12 17:49:29 +0300

    add navigation menu

---
+++ app/src/main/java/com/example/nfcproject/navigationMenu.kt
@@ -0,0 +1,27 @@
+package com.example.nfcproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import com.example.nfcproject.databinding.FragmentNavigationMenuBinding
+
+class navigationMenu : Fragment() {
+
+    private lateinit var binding:FragmentNavigationMenuBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNavigationMenuBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val drawer = binding.navigdtionMenu
+        drawer.openDrawer(GravityCompat.START);
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
