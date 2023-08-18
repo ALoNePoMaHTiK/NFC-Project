a7ea76bd6d8460d578c626073edf47dc9e8452f9
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-08-18 18:31:57 +0300

    UI last version

--- app/src/main/java/com/example/nfcproject/navigationMenu.kt
+++ app/src/main/java/com/example/nfcproject/navigationMenu.kt
@@ -1,16 +1,22 @@
 package com.example.nfcproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.nfcproject.databinding.FragmentNavigationMenuBinding
+import com.google.android.material.navigation.NavigationView
 
-class navigationMenu : Fragment() {
+class navigationMenu : Fragment(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding:FragmentNavigationMenuBinding
+    private lateinit var  navigationView: NavigationView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,7 +27,23 @@ class navigationMenu : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val drawer = binding.navigationMenu
-        drawer.openDrawer(GravityCompat.START);
+        drawer.openDrawer(GravityCompat.START)
+        navigationView = binding.NavigationVew
+        navigationView.setNavigationItemSelectedListener(this)
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val navView: NavigationView = binding.NavigationVew
+        val navController = findNavController(binding.FragmentContainerView)  // находим NavController
+        navView.setupWithNavController(navController)
+        when (item.getItemId()) {
+            R.id.profile -> {
+                Log.d("NFCProjectTestDebug", "profile")
+
+            }
+            else -> false
+        }
+        return true
     }
 }
